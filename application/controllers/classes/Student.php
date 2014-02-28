29ed21b34c736a6a60f3f460498803413b3d931a
VashX30a <vashX30a@gmail.com>
2014-02-28 11:21:27 +0800

    additions
    added getters

--- application/controllers/classes/Student.php
+++ application/controllers/classes/Student.php
@@ -1,8 +1,15 @@
 <?php
 
 	class Student {
+	
+		private $CI;
 		
 		public function __construct() {
-			//TODO
+			$this->CI =& get_instance();
+			$this->CI->load->model('student_model','SM');
+		}
+		
+		public function isInDatabase($idNumber){
+		
 		}
 	}
---
+++ application/models/student_model.php
@@ -0,0 +1,74 @@
+<?php   if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class student_model extends CI_Model {
+	
+	public function __construct() {
+		parent::__construct();
+		$this->load->database();
+	}
+	
+	public function isInDatabase($idNumber)
+	{
+	
+		$query = $this->db->query("	SELECT studid
+									FROM student
+									WHERE studid = '$idNumber'
+									");
+		return $query;
+	}
+	
+	public function getStudPhone($idNumber){
+		
+		$query = $this->db->query(" SELECT studphone 
+									FROM student 
+									WHERE studid = '$idNumber'
+									");
+		return $query;
+	}
+	
+	public function getStudCourse($idNumber){
+		
+		$query = $this->db->query(" SELECT course
+									FROM student 
+									WHERE studid = '$idNumber'
+									");
+		return $query;
+	}
+
+	public function getStudCollege($idNumber){
+		
+		$query = $this->db->query(" SELECT college 
+									FROM student 
+									WHERE studid = '$idNumber'
+									");
+		return $query;
+	}
+	
+	public function getStudLName($idNumber){
+		
+		$query = $this->db->query(" SELECT lastname
+									FROM student 
+									WHERE studid = '$idNumber'
+									");
+		return $query;
+	}
+	
+	public function getStudGName($idNumber){
+		
+		$query = $this->db->query(" SELECT givenname 
+									FROM student 
+									WHERE studid = '$idNumber'
+									");
+		return $query;
+	}
+	
+	public function getStudMName($idNumber){
+		
+		$query = $this->db->query(" SELECT middlename 
+									FROM student 
+									WHERE studid = '$idNumber'
+									");
+		return $query;
+	}
+}
+
