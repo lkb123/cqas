f1d557b6a8726466ffa5d9c941ee882b4776e4f7
VashX30a <vashX30a@gmail.com>
2014-03-10 09:59:14 +0800

    commit

--- application/views/cashier/cashier_login.php
+++ application/views/cashier/cashier_login.php
@@ -14,7 +14,7 @@
 	<br><br>
 	</font>
 
-	<form method = "post" action = <?php echo site_url() . '/mainframe/login' ?>>
+	<form method = "post" action = "cashier_home.php">
 		Cashier ID: <input type="text" name = "cashierid" />
 		<br>
 		Password; <input type="password" name="cashierpass" />
