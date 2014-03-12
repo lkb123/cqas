93de90921f3f0e51594eb9355ff37342b25e681e
VashX30a <vashX30a@gmail.com>
2014-03-13 00:45:14 +0800

    login
    initial

--- application/views/cashier/cashier_login.php
+++ application/views/cashier/cashier_login.php
@@ -14,7 +14,7 @@
 	<div class="row">
     	<div class="container" id="formContainer">
 
-          <form action="<?php echo site_url() . '/mainframe/login';?>"class="form-signin" id="login" role="form">
+          <form action="<?php echo site_url('mainframe/login');?>"class="form-signin" id="login" role="form">
             <h3 class="form-signin-heading">Cashier Login</h3>
             <input type="text" class="form-control" name="cashierid" id="cashierid" placeholder="Username" required autofocus>
             <input type="password" class="form-control" name="cashierpass" id="cashierpass" placeholder="Password" required>
