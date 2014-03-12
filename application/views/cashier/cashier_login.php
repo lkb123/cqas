33cb39b2c88bd9d9ccc3d77f60d9abb80ccdb466
VashX30a <vashX30a@gmail.com>
2014-03-13 00:28:08 +0800

    views
    removed unnecessary files

--- application/views/cashier/cashier_login.php
+++ application/views/cashier/cashier_login.php
@@ -7,49 +7,21 @@
 				echo $message;
 		?>
 		</font>
-		<link rel="stylesheet" href="css/bootstrap.css"/>
-		<link rel="stylesheet" href="css/bootstrap.min.css"/>
-		<div class="container">    
-			<div id="loginbox" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">                    
-				<div class="panel panel-info" >
-						<div class="panel-heading">
-							<div class="panel-title">Cashier Login</div>
-						</div>     
+		
+		
 
-						<a href=<?php echo site_url() . '/mainframe/index' ?>> Home </a>
-						
-						<div style="padding-top:30px" class="panel-body" >
+<div class="container">
+	<div class="row">
+    	<div class="container" id="formContainer">
 
-							<div style="display:none" id="login-alert" class="alert alert-danger col-sm-12"></div>
-                            
-							<form method = post action=<?php echo site_url() . '/mainframe/login' ?> id="loginform" class="form-horizontal" role="form">
-                                    
-								<div style="margin-bottom: 25px" class="input-group">
-											<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-											<input id="login-username" type="text" class="form-control" name="cashierid" value="" placeholder="username">                                        
-										</div>
-                                
-								<div style="margin-bottom: 25px" class="input-group">
-											<span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-											<input id="login-password" type="password" class="form-control" name="cashierpass" placeholder="password">
-										</div>
+          <form action="<?php echo site_url() . '/mainframe/login';?>"class="form-signin" id="login" role="form">
+            <h3 class="form-signin-heading">Cashier Login</h3>
+            <input type="text" class="form-control" name="cashierid" id="cashierid" placeholder="Username" required autofocus>
+            <input type="password" class="form-control" name="cashierpass" id="cashierpass" placeholder="Password" required>
+            <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+          </form>
+    
 
-
-									<div style="margin-top:10px" class="form-group">
-										<!-- Button -->
-
-										<div class="col-sm-12 controls">
-										<input id="btn-login" href="#" class="btn btn-success" type=submit>
-
-										</div>
-									</div>
-
-								</form>     
-
-
-
-							</div>                     
-						</div>  
-					</div>    
-			</div> 
-		</div>
+        </div> <!-- /container -->
+	</div>
+</div>
