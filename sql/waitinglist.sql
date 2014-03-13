a96bfadaca89ad9abf8adab0362a200e8b5cc3eb
lkb123 <louiebasay@gmail.com>
2014-03-13 23:49:18 +0800

    updated sql schemas

--- sql/waitinglist.sql
+++ sql/waitinglist.sql
@@ -1,9 +1,11 @@
 CREATE TABLE waitinglist (
     studid varchar(10),
     prioritynumber integer NOT NULL,
-	timestampadded timestamp NOT NULL,
-	timestampserved timestamp default null,
+	dateadded date NOT NULL,
+	timeadded time NOT NULL,
+	dateserved date default null,
+	timeserved time NOT NULL,
 	served boolean default false,
-	primary key(studid, timestampadded),
+	primary key(studid, dateadded, timeadded),
 	foreign key(studid) references student(studid) on update cascade on delete cascade
 );
