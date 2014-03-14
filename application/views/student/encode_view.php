bba40fe1264e03d6401166bb29b7b44dc0de6697
Jalil Laguindab <wnxenero@yahoo.com>
2014-03-14 12:30:17 +0800

    Commit

--- application/views/student/encode_view.php
+++ application/views/student/encode_view.php
@@ -1,49 +1,22 @@
 		
-	<div class = "container">
+<div class = "container">
 
-		<button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
-		  TEST MODAL
-		</button>
-
-		<!-- Modal -->
-		<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-		  <div class="modal-dialog">
-			<div class="modal-content">
-			  <div class="modal-header">
-				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-				<h4 class="modal-title" id="myModalLabel">Modal title</h4>
-			  </div>
-			  <div class="modal-body">
-				...
-			  </div>
-			  <div class="modal-footer">
-				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-				<button type="button" class="btn btn-primary">Save changes</button>
-			  </div>
-			</div>
-		  </div>
-		</div>
-
-	<font color = "red">
-
-	<br><br>
-	
-	<div class="alert alert-danger">
 	<?php 
 			if($message == '')
 				echo '';
 			else
+				echo '<div class="alert alert-danger">';
 				echo $message;
+				echo '</div>';
 	?>
-	</div>
-	</font>
+
 	<form method=post action=<?php echo site_url() . '/mainframe/encode' ?>>
 	ID Number: 
 	<input type=text name="idNumber" />
 	<input type="checkbox" name="subscribe" value="true" />Subscribe to Alert System
-	<br>	
+	
 	<input type=submit value="submit" />
-	<br><br>
+
 	<a href=<?php echo site_url() . '/mainframe/index' ?>>Reset Priority Number to 0 </a> <br>
 	</form>
-	</div>
+</div>
