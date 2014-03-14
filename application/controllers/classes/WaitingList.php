63827161bacf9184fbd3299549718a8ec708728b
VashX30a <vashX30a@gmail.com>
2014-03-14 14:30:55 +0800

    result_array

--- application/controllers/classes/WaitingList.php
+++ application/controllers/classes/WaitingList.php
@@ -61,9 +61,9 @@
 		}
 
 		public function retrieveFifteenStudents() {
-			$data['query_result'] = $this->CI->WM->getFifteenStudents();
-			//echo var_dump($data['query_result']);
-			return $data;
+			$data = $this->CI->WM->getFifteenStudents();
+			//echo var_dump($data->result_array());
+			return $data->result_array();
 		}
 
 		/*
