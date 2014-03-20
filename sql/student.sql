e126b68e4e16255c75c8d51307da901cd28fc498
VashX30a <vashX30a@gmail.com>
2014-03-20 14:07:37 +0800

    commit

--- sql/student.sql
+++ sql/student.sql
@@ -1,6 +1,6 @@
 CREATE TABLE student (
     studid character varying(10) NOT NULL primary key,
-    studphone character varying(12),
+    studphone character varying(15),
     lastname varchar(20),
     givenname varchar(50),
     middlename varchar(20),
