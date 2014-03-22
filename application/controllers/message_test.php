a1481f1414926b7169131e37ea35b470f330a139
lkb123 <louiebasay@gmail.com>
2014-03-22 11:55:48 +0800

    add part 2

---
+++ application/controllers/message_test.php
@@ -0,0 +1,28 @@
+<?php
+
+	include(basename(dirname('classes/Message.php')) . '/Message.php');
+
+	class message_test extends CI_Controller {
+
+		private $message;
+		public function __construct() {
+			parent::__construct();
+			$this->load->library('unit_test');
+			$this->message = new Message();
+		}
+		
+		public function messageAler1Test() {
+			$expected = "9 more students and it will be your turn to be served please proceed to the cashier";
+			$result = $this->message->messageAlertFor10thStudent();
+			$this->unit->run($result, $expected);
+			$this->load->view('test');
+		}
+
+		public function messageAler2Test() {
+			$expected = "49 more students and it will be your turn to be served please proceed to the cashier";
+			$result = $this->message->messageAlertFor50thStudent();
+			$this->unit->run($result, $expected);
+			$this->load->view('test');
+		}
+		
+	}
