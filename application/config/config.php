23b62ce1145edb19e65568897eb33befb89cd51f
Jalil Laguindab <wnxenero@yahoo.com>
2014-03-29 02:29:51 +0800

    commit

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url'] = "http://localhost:1234/cqas/";
+$config['base_url'] = "http://localhost/cqas/";
 
 /*
 |--------------------------------------------------------------------------
